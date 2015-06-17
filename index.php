9b6e7d20ed0f1fd9eeb4294953e8e12c2c37d749
hipercubus <subespacios@gmail.com>
2015-06-17 17:41:26 -0300

    html

--- index.php
+++ index.php
@@ -14,6 +14,23 @@
 				<img src="logo_320x50.jpg"/>
 			</div>
 		</div>	
+		<div class="row">
+			<div class="col-md-12">
+<?php
+$feed = implode(file('http://feeds.feedburner.com/148apps'));
+$xml = simplexml_load_string($feed);
+$json = json_encode($xml);
+$array = json_decode($json,TRUE);
+
+// echo $json;
+?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="tag-320x50 center-block">
+<script type="text/javascript" src="http://ad.tmlbox.co/ad/9480/tag.js?pubid=G4M&mediatype=mobileweb&channel=IAB1&channel=IAB12&channel=IAB6&channel=IAB18"></script>
+			</div>
+		</div>
 	</div>
 </body>
 </html>

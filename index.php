b28687ec15e8bc723a484ddaff0fa150fe3de895
hipercubus <subespacios@gmail.com>
2015-06-17 16:41:09 -0300

    html

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<img src="logo_320x50.jpg"/>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>games4mobi.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<img src="logo_320x50.jpg"/>
+	<div style="color:#222">cupala puto</div>
+</body>
